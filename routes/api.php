78a9001610deb9f22aa1e892866d3aef620b3b7d
OrjoanAli <orjoanaldabaiba@gmail.com>
2026-04-18 18:52:03 +0300

    feat: add categories and services API with standardized responses

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\LoginUserController;
 use App\Http\Controllers\RegisterUserController;
+use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\ServiceController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -12,3 +14,9 @@ Route::get('/user', function (Request $request) {
 Route::post('/register', [RegisterUserController::class, 'addNewUser']);
 
 Route::post('/logIn', [LoginUserController::class, 'logIn']);
+
+Route::get('/categories', [CategoryController::class, 'index']);
+
+Route::get('/services', [ServiceController::class, 'index']);
+
+Route::get('/services/{id}', [ServiceController::class, 'show']);

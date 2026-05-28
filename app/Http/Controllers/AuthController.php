1db58db29963628659832653747bab8e402323e0
OrjoanAli <orjoanaldabaiba@gmail.com>
2026-05-28 17:48:24 +0300

    feat: use 6-digit verification code for password reset

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -98,7 +98,8 @@ class AuthController extends Controller
         DB::table('password_reset_tokens')->updateOrInsert(
             ['email' => $request->email],
             [
-                'token' => Hash::make($token),
+                //'token' => Hash::make($token),// i think we should put this instead 'token' => $token,
+                'token' => $token,
                 'created_at' => now()
             ]
         );
